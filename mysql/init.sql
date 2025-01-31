a690524b865ec57960516ad0cf485e3182322e91
Mark Villanueva <nokiev@yahoo.com>
2025-01-31 21:56:27 +0800

    updated docker-compose with 3 teamcity-agents

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,5 +1,13 @@
+-- Create the TeamCity database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS teamcity;
+
+-- Create the TeamCity user if it doesn't exist and grant necessary privileges
 CREATE USER IF NOT EXISTS 'teamcity'@'%' IDENTIFIED BY 'teamcity_password';
 GRANT ALL PRIVILEGES ON teamcity.* TO 'teamcity'@'%';
 FLUSH PRIVILEGES;
+
+-- Use the TeamCity database
+USE teamcity;
+
+-- Add any required table creation scripts for TeamCity here
 
